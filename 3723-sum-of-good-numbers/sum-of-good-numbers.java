18a722dd38db5fda91bab53aeef27de595bb7683
Nikhil Kaparaju <nikhil.22bce7144@vitapstudent.ac.in>
2025-09-12 23:06:56 +0530

    Time: 1 ms (99.81%) | Memory: 44.8 MB (8.40%) - LeetSync

---
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int sumOfGoodNumbers(int[] nums, int k) {
+        
+        int n = nums.length;
+        int count = 0 ;
+        
+        for (int i = 0; i < n; i++) {
+            
+            boolean good = true;
+            
+            if (i - k >= 0 && nums[i] <= nums[i - k]) {
+                good = false;
+            }
+
+            if (i + k < n && nums[i] <= nums[i + k]) {
+                good = false;
+            }
+
+            if (good) {
+                count += nums[i];
+            }
+        }
+        
+        return count;
+    }
+}
