bd39ea8681b06087a9bf63bc2f8ba9db3077c815
Nikhil Kaparaju <nikhil.22bce7144@vitapstudent.ac.in>
2025-12-30 01:57:12 +0530

    Time: 4 ms (78.77%) | Memory: 44.9 MB (5.98%) - LeetSync

---
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum=0;
+        for(int i=0;i<apple.length;i++){
+            sum+=apple[i];
+        }
+        Arrays.sort(capacity);
+        int box=0;
+        int ans=0;
+        for(int i=capacity.length-1;i>=0;i--){
+            box+=capacity[i];
+            ans++;
+            if(box>=sum){
+                break;
+            }
+        }
+        return ans;
+    }
+}
